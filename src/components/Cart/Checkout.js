cc59c6bff1f433b3dd17d685c2d64183e60fef3b
mahovsson <matus137@gmail.com>
2021-04-28 18:01:10 +0200

    hook connected to name input

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,15 +1,46 @@
 import classes from './Checkout.module.css';
+import useInput from '../hooks/use-input';
 
 const Checkout = (props) => {
+	const {
+		value: inputName,
+		inputValueIsValid: inputNameIsValid,
+		hasError: inputNameHasError,
+		inputChangeHandler: nameChangeHandler,
+		inputBlurHandler: nameBlurHandler,
+		inputReset: nameReset,
+	} = useInput((value) => value.trim() !== '');
+
+	let formIsValid = false;
+
+	if (inputNameIsValid) {
+		formIsValid = true;
+	}
+
 	const confirmHandler = (event) => {
 		event.preventDefault();
+
+		nameReset();
 	};
+
+	const nameInputClasses = `${classes.control} ${
+		inputNameHasError ? classes.invalid : ''
+	}`;
 
 	return (
 		<form onSubmit={confirmHandler}>
-			<div className={classes.control}>
+			<div className={nameInputClasses}>
 				<label htmlFor='name'>Your Name</label>
-				<input type='text' id='name' />
+				<input
+					type='text'
+					id='name'
+					value={inputName}
+					onChange={nameChangeHandler}
+					onBlur={nameBlurHandler}
+				/>
+				{inputNameHasError && (
+					<p className={classes.error}>Please insert valid name</p>
+				)}
 			</div>
 			<div className={classes.control}>
 				<label htmlFor='street'>Street</label>
@@ -27,7 +58,12 @@ const Checkout = (props) => {
 				<button type='button' onClick={props.onCancel}>
 					Cancel
 				</button>
-				<button className={classes.submit}>Confirm</button>
+				<button
+					disabled={!formIsValid}
+					className={!formIsValid ? '' : classes.submit}
+				>
+					Confirm
+				</button>
 			</div>
 		</form>
 	);
