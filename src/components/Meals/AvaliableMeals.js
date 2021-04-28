eb7e86113bb39cc2c36b49a56a52b09c6593891d
mahovsson <matus137@gmail.com>
2021-04-28 15:03:31 +0200

    Loading state

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -4,48 +4,60 @@ import classes from './AvaliableMeals.module.css';
 import MealItem from './MealItem';
 import Card from '../UI/Card';
 
-
 const AvaliableMeals = () => {
-  const [ meals, setMeals ] = useState([]);
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState('');
 
-  const fetchData = async () => {
-  let response = await fetch('https://food-order-app-3b76f-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
-  let data = await response.json();
-  let transformedData = [];
+	const fetchData = async () => {
+		setIsLoading(true);
+		let response = await fetch(
+			'https://food-order-app-3b76f-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+		);
+		let data = await response.json();
+		let transformedData = [];
 
-  for(const key in data ) {
-      transformedData.push({
-        id: key,
-        name: data[key].name,
-        description: data[key].description,
-        price:data[key].price
-      });
-  }
-  setMeals(transformedData);
-}
+		for (const key in data) {
+			transformedData.push({
+				id: key,
+				name: data[key].name,
+				description: data[key].description,
+				price: data[key].price,
+			});
+		}
+		setMeals(transformedData);
+		setIsLoading(false);
+	};
 
-useEffect(() => {
-  fetchData();
-}, [])
+	useEffect(() => {
+		fetchData();
+	}, []);
 
+	if (isLoading) {
+		return (
+			<section className={classes['meal-loading']}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
 
-  const mealsList = meals.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+	const mealsList = meals.map((meal) => (
+		<MealItem
+			key={meal.id}
+			id={meal.id}
+			name={meal.name}
+			description={meal.description}
+			price={meal.price}
+		/>
+	));
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+	return (
+		<section className={classes.meals}>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
+		</section>
+	);
 };
 
 export default AvaliableMeals;
